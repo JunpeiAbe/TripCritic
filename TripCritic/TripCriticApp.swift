22583cfe4364d2f3c23f1ad2dfcbc90bae21a644
JunpeiAbe <uihrsw0228@gmail.com>
2024-08-04 16:35:54 +0900

    Initial Commit

---
+++ TripCritic/TripCriticApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripCriticApp.swift
+//  TripCritic
+//
+//  Created by j on 2024/08/04.
+//
+
+import SwiftUI
+
+@main
+struct TripCriticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
