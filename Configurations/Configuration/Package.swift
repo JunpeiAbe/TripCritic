db8d136dd3bb143a2e2ff4db9d418151c9f442b1
JunpeiAbe <uihrsw0228@gmail.com>
2024-08-07 16:06:18 +0900

    Configurationのタイポ修正

--- Configurations/Configuration/Package.swift
+++ Configurations/Configuration/Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Configration",
+    name: "Configuration",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Configration",
-            targets: ["Configration"]),
+            name: "Configuration",
+            targets: ["Configuration"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Configration"),
+            name: "Configuration"),
         .testTarget(
-            name: "ConfigrationTests",
-            dependencies: ["Configration"]),
+            name: "ConfigurationTests",
+            dependencies: ["Configuration"]),
     ]
 )
