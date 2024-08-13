9a1014cfe9fae6f28a01ce6aaaac8e584bed9438
JunpeiAbe <89124336+JunpeiAbe@users.noreply.github.com>
2024-08-13 16:55:36 +0900

    Revert "Feature/common api add"

--- CoreModules/APIRequests/Sources/APIRequests/GetAppInfo/GetAppInfoAPI.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-/// アプリ情報取得API
-public struct GetAppInfoAPI: GetRequestable {
-    public typealias Response = GetAppInfoResponse
-    
-    public let path: String = "AppInfo"
-    
-    public init() {}
-}
--- CoreModules/Models/Sources/Models/APIStatus.swift
+++
@@ -1,5 +0,0 @@
-///APIステータス
-public enum APIStatus: String, Decodable {
-    case success = "success"
-    case error = "error"
-}
