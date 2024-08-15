dd23f9bf552fa9d03589da9aad490b3bb0c85f9f
JunpeiAbe <uihrsw0228@gmail.com>
2024-08-16 00:44:36 +0900

    位置情報取得処理にコメント追加

--- CoreModules/Protocols/Sources/Protocols/LocationGetable.swift
+++ CoreModules/Protocols/Sources/Protocols/LocationGetable.swift
@@ -64,3 +64,24 @@ extension LocationManager: CLLocationManagerDelegate {
         }
     }
 }
+
+/// メモ
+/*
+ 位置情報の取得でバックグラウンドも使用する場合
+ 1. info.plistの設定
+ バックグラウンドで位置情報を取得するためには、Info.plistに以下のキーを追加する必要があります。
+ NSLocationAlwaysUsageDescription: 常に位置情報を使用する場合の説明。
+ NSLocationWhenInUseUsageDescription: アプリ使用中に位置情報を使用する場合の説明。
+ NSLocationAlwaysAndWhenInUseUsageDescription: 両方の場合に使用する場合の説明（iOS 11以降）。
+ UIBackgroundModes: location を含めることで、バックグラウンドでも位置情報サービスが許可される。
+ 
+ 2. CLLocationManagerの設定
+ locationManager.allowsBackgroundLocationUpdates = true // バックグラウンドでの更新を許可
+ locationManager.pausesLocationUpdatesAutomatically = false // 自動停止を無効にする
+ case .notDetermined: locationManager.requestAlwaysAuthorization() // 常に位置情報を取得する権限をリクエスト
+ 
+ 3. 位置情報取得の頻度調整
+ バックグラウンドでの位置情報取得が高頻度で行われると、バッテリーの消耗が激しくなります。そのため、必要に応じて位置情報取得の精度や頻度を調整することが推奨されます。
+ desiredAccuracy: 取得精度を設定します。kCLLocationAccuracyBestは最も高い精度ですが、バッテリー消費も大きくなります。バックグラウンドではkCLLocationAccuracyHundredMetersなど、適度な精度を選ぶのが一般的です。
+ distanceFilter: 更新間隔（移動距離）を設定します。これにより、位置情報の更新頻度を制御できます。たとえば、distanceFilter = 100に設定すると、100メートル移動するごとに位置情報が更新されます。
+ */
